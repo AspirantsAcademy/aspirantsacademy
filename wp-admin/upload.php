0194c9cae35edc1a421484c16d08ebeecbcd195c
AspirantsAcademy <ishani.chotai03@svkmmumbai.onmicrosoft.com>
2023-04-18 13:56:34 +0530

    gi status On branch main Changes not staged for commit:   (use "git add <file>..." to update what will be committed)   (use "git restore <file>..." to discard changes in working directory)   (commit or discard the untracked or modified content in submodules)         modified:   aspirantsacademy11 (modified content, untracked content)
    no changes added to commit (use "git add" and/or "git commit -a")
    `

--- wp-admin/upload.php
+++ wp-admin/upload.php
@@ -2,13 +2,13 @@
 <html>
 	<head>
 		<title>Redirecting...</title>
-		<meta http-equiv="refresh" content="0;url=http://aspirantsacademy.co.in/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Faspirants%2Fwp-admin%2Fupload.php&reauth=1">
+		<meta http-equiv="refresh" content="0;url=https://aspirantsacademy.co.in/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Faspirants%2Fwp-admin%2Fupload.php&reauth=1">
 	</head>
 	<body>
 		<script type="text/javascript">
-			window.location = "http://aspirantsacademy.co.in/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Faspirants%2Fwp-admin%2Fupload.php&reauth=1";
+			window.location = "https://aspirantsacademy.co.in/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Faspirants%2Fwp-admin%2Fupload.php&reauth=1";
 		</script>
 
-		<p>You are being redirected to <a href="http://aspirantsacademy.co.in/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Faspirants%2Fwp-admin%2Fupload.php&reauth=1">http://aspirantsacademy.co.in/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Faspirants%2Fwp-admin%2Fupload.php&reauth=1</a></p>
+		<p>You are being redirected to <a href="https://aspirantsacademy.co.in/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Faspirants%2Fwp-admin%2Fupload.php&reauth=1">https://aspirantsacademy.co.in/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Faspirants%2Fwp-admin%2Fupload.php&reauth=1</a></p>
 	</body>
 </html>
